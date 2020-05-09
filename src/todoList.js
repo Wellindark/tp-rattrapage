d801efab4f41787b346398acaa3c3f5e78cc1814
Unknown <r.marquilly@gmail.com>
2020-05-09 15:16:40 +0200

    Add Todo
    Can Add todo to the list of todo

--- src/todoList.js
+++ src/todoList.js
@@ -1,18 +1,24 @@
 import React, {Component} from "react";
 import Todo from "./todo";
+import AddTodo from "./addTodo";
 
 class todoList extends Component{
     state={
-        todos:[
-            {id:1, content:"Acheter du lait"},
-            {id:2, content:"Jouer a valorant"}
-        ]
+        todos:[]
     }
+
+    addTodo =(todo) => {
+        todo.id = Math.random();
+        let todos = [...this.state.todos, todo]
+        this.setState({todos: todos})
+    }
+    
     render(){
         return(
             <div className="TodoList">
                 <h1 className="center blue-text">Todo's</h1>
                 <Todo todos={this.state.todos}/>
+                <AddTodo addTodo={this.addTodo}/>
             </div>
         );
     }
